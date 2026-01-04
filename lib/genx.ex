9024a26b7e88f823219416abf1fa26a7c06800a0
Chad Fennell <chadfennell@users.noreply.github.com>
2026-01-04 11:21:53 -0700

    initial commit

---
+++ lib/genx.ex
@@ -0,0 +1,18 @@
+defmodule Genx do
+  @moduledoc """
+  Documentation for `Genx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Genx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/genx_test.exs
@@ -0,0 +1,8 @@
+defmodule GenxTest do
+  use ExUnit.Case
+  doctest Genx
+
+  test "greets the world" do
+    assert Genx.hello() == :world
+  end
+end
